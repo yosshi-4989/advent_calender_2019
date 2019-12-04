59f0a2ce7f53dcb8d01f56b15c95c68d4722abfc
yoshida <archer0729@icloud.com>
2019-12-05 00:46:09 +0900

    12/05 毎回ローディングが発生しないようにと詳細画面でデータを取得できるまでダミー文言を表示するように修正

--- wp-tutorial/src/app/home/home.page.ts
+++ wp-tutorial/src/app/home/home.page.ts
@@ -25,12 +25,22 @@ export class HomePage {
     const loading = await this.loadingController.create({
       message: 'Loading...',
     });
-    await loading.present();
+    if (!this.posts.length) {
+      // データが存在しない場合、読み込み完了までローディングを表示
+      await loading.present();
+    }
     this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/')
       .subscribe(data => {
         this.posts = data['posts'];
         // ローディング画面の非表示
         loading.dismiss();
       });
+  }
+
+  // trackByに渡す関数
+  // *ngForでtrackByにメソッドを指定すると、postsの変更で再描画する際に
+  // 返却する値を参照して値の修正が入るようになる？
+  trackByFn(index, item): number {
+    return item.ID;
   }
 }
