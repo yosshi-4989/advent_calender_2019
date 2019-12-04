e407d9cf94d0089f5b9cbb05ff85e6d12708dece
yoshida <archer0729@icloud.com>
2019-12-04 23:24:41 +0900

    12/05 記事一覧表示画面の実装

--- wp-tutorial/src/app/home/home.page.ts
+++ wp-tutorial/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  }[] = [];
 
-  constructor() {}
+  constructor(
+    public http: HttpClient,
+    public loadingController: LoadingController,
+  ) {}
 
+  async ionViewDidEnter() {
+    // ローディング画面の作成
+    const loading = await this.loadingController.create({
+      message: 'Loading...',
+    });
+    await loading.present();
+    this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/')
+      .subscribe(data => {
+        this.posts = data['posts'];
+        // ローディング画面の非表示
+        loading.dismiss();
+      });
+  }
 }
