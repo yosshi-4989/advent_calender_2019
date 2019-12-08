4b9667c8b13b6121532823dcee20d0933f34ef1e
yoshida <archer0729@icloud.com>
2019-12-09 01:03:21 +0900

    12/09 ログイン状態に応じたアクセス制御を追加

--- chat-tutorial/src/app/app-routing.module.ts
+++ chat-tutorial/src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// 未認証時に遷移するURLを設定
+const redirectUnauthorised = () => redirectUnauthorizedTo(['auth/signin']);
+// 認証時に遷移するURLを設定
+const redirectLoggedIn = () => redirectLoggedInTo(['/']);
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [AngularFireAuthGuard],
+    data: {authGuardPipe: redirectUnauthorised},
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+    canActivate: [AngularFireAuthGuard],
+    data: {authGuardPipe: redirectLoggedIn}
   }
 ];
 @NgModule({
