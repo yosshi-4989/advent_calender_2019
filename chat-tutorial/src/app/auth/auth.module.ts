8db562f1b2ecda187c4b9ee21f58e5899b34297e
yoshida <archer0729@icloud.com>
2019-12-08 23:36:08 +0900

    12/09 モジュールの統一

--- chat-tutorial/src/app/auth/auth.module.ts
+++ chat-tutorial/src/app/auth/auth.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { SignupPage } from './signup/signup.page';
+import { SigninPage } from './signin/signin.page';
 
-
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupPage
+  },
+  {
+    path: 'signin',
+    component: SigninPage
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [SignupPage, SigninPage],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthModule { }
