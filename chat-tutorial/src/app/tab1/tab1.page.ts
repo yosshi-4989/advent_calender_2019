f80e86388acccb601372d4a09b503f7984d211cd
yoshida <archer0729@icloud.com>
2019-12-10 23:50:21 +0900

    12/11 タブ、プロフィール画面の調整とチャット画面のモックアップ

--- chat-tutorial/src/app/tab1/tab1.page.ts
+++ chat-tutorial/src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ProfilePage } from '../shared/profile/profile.page';
+import { AuthService } from '../auth/auth.service';
+import { FirestoreService } from '../shared/firestore.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,18 +10,24 @@ import { ProfilePage } from '../shared/profile/profile.page';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-
+  message = '';
   constructor(
     public modalController: ModalController,
+    public auth: AuthService,
+    public firestore: FirestoreService,
   ) {}
 
   async ngOnInit() {
-    // modalを作成
-    const modal = await this.modalController.create({
-      component: ProfilePage,
-    });
-    // modalを表示
-    await modal.present();
+    // authからuidを取得し、firestoreのデータを確認
+    const user = await this.firestore.userInit( this.auth.getUserId() );
+    if (!user) {
+      // modalを作成
+      const modal = await this.modalController.create({
+        component: ProfilePage,
+      });
+      // modalを表示
+      await modal.present();
+    }
   }
 
 }
