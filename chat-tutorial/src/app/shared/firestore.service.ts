d3d64eb9267c3fc1f070e21db66f2eafb6ec6b90
yoshida <archer0729@icloud.com>
2019-12-11 01:23:11 +0900

    12/11 チャット機能実装

--- chat-tutorial/src/app/shared/firestore.service.ts
+++ chat-tutorial/src/app/shared/firestore.service.ts
@@ -1,19 +1,61 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
-import { first } from 'rxjs/operators';
+import { AngularFirestoreDocument, AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { first, concatMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 export interface IUser {
   displayName: string;
   photoDataUrl: string;
 }
 
+// メッセージ情報を格納する型
+export interface IMessage {
+  uid: string;
+  message: string;
+  timestamp: number;
+}
+// ユーザー、メッセージの両方の要素を持つクラス型
+export interface IChat extends IUser, IMessage {}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
   userDoc: AngularFirestoreDocument<IUser>;
+  messageCollection: AngularFirestoreCollection<IMessage>;
+  userCollection: AngularFirestoreCollection<IUser>;
 
-  constructor(public af: AngularFirestore) { }
+  constructor(public af: AngularFirestore) {
+    // メッセージのコレクションを時刻順に取得
+    this.messageCollection = this.af.collection<IMessage>('chat', ref => ref.orderBy('timestamp', 'desc'));
+    // ユーザのコレクションを取得
+    this.userCollection = this.af.collection<IUser>('users');
+  }
+
+  // メッセージのコレクションにメッセージを追加
+  messageAdd(message: IMessage) {
+    return this.messageCollection.add(message);
+  }
+
+  // メッセージのコレクションから表示するIChat[]を生成する
+  // PromiseではなくRxJSのObservable型で利用していることでメッセージのコレクションが更新されるとデータ取得が走る(ストリームが流れる、というらしい)
+  chatInit(): Observable<IChat[]> {
+    // メッセージコレクションのvalueを取得(DocumentのidをmessageIdとして追加)
+    return this.messageCollection.valueChanges( {idField: 'messageId'} )
+      // コレクションのメッセージ順に処理する
+      .pipe(concatMap(async messages => {
+        // ユーザーコレクションのvalueを取得(Documentのidをuidとして追加)
+        const users = await this.userCollection.valueChanges( {idField: 'uid'} )
+          .pipe(first()).toPromise(Promise);
+        // IMessage[]からIChat[]を生成して返却
+        return messages.map(message => {
+          // messageのuidと一致するuserを取得
+          const user = users.find(u => u.uid === message.uid);
+          // message(IMessage型)にuser(IUser型)の要素を追加してIChat型のオブジェクトを生成
+          return Object.assign(message, user);
+        });
+      }));
+  }
 
   // ユーザー情報(userDoc)を初期化するメソッド
   userInit(uid: string): Promise<IUser> {
--- chat-tutorial/src/app/tab1/tab1.page.ts
+++ chat-tutorial/src/app/tab1/tab1.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, IonContent } from '@ionic/angular';
 import { ProfilePage } from '../shared/profile/profile.page';
 import { AuthService } from '../auth/auth.service';
-import { FirestoreService } from '../shared/firestore.service';
+import { FirestoreService, IUser, IChat } from '../shared/firestore.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -11,6 +12,13 @@ import { FirestoreService } from '../shared/firestore.service';
 })
 export class Tab1Page implements OnInit {
   message = '';
+  uid: string;
+  user: IUser;
+  chat: Observable<IChat[]>;
+
+  // contentプロパティを<ion-content>に紐づける
+  @ViewChild(IonContent, { static: true})
+  content: IonContent;
   constructor(
     public modalController: ModalController,
     public auth: AuthService,
@@ -27,7 +35,40 @@ export class Tab1Page implements OnInit {
       });
       // modalを表示
       await modal.present();
+      // モーダルが非表示になった時にも表示するメソッドを実行してユーザー情報を取得する
+      modal.onWillDismiss().then(() => this.ionViewWillEnter());
     }
+    // chatを初期化
+    this.chat = this.firestore.chatInit();
   }
 
+  // ユーザ情報を取得
+  async ionViewWillEnter() {
+    this.uid = this.auth.getUserId();
+    this.user = await this.firestore.userInit(this.uid);
+  }
+
+  // メッセージの送信
+  postMessage() {
+    // ユーザー情報がない場合ははじく
+    if (!this.user) {
+      alert('プロフィール登録が必要です');
+      return;
+    }
+    // メッセージの送信
+    this.firestore.messageAdd({
+      uid: this.uid,
+      message: this.message,
+      timestamp: Date.now(),
+    });
+    // 入力の初期化
+    this.message = '';
+    // 100msかけて一番上までスクロールする
+    this.content.scrollToTop(100);
+  }
+
+  // チャットが更新されたときに全部再描画されないようにmessageIdで区別する
+  trackByFn(index, item) {
+    return item.messageId;
+  }
 }
